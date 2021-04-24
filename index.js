03bb490ed7b13d0526d90c25a8cb8979a4b29d6c
tuolomee <kejmarova@gmail.com>
2021-04-24 20:22:56 +0200

    cyklus, střídání hráčů, vkládání obr do pole

--- index.js
+++ index.js
@@ -6,8 +6,24 @@ const btnElm = document.querySelectorAll('.game_area--btn');
 const hrac = document.querySelector('.player')
 
 const Hraje = () => {
-  return `<p class="player">Hraje: <img class="circle"src="${kdoJeNaTahu}svg"alt="circle"></p>`;
+  return `<p class="player">Hraje: <img class="${kdoJeNaTahu}"src="images/${kdoJeNaTahu}.svg"alt="${kdoJeNaTahu}"></p>`;
 };
 
 hrac.innerHTML = Hraje(kdoJeNaTahu);
 
+for (let i = 0; i < btnElm.length; i+=1) {
+  btnElm[i].addEventListener('click', () => {
+  if (kdoJeNaTahu === 'circle') {
+    btnElm[i].classList.add('pole-circle');
+    kdoJeNaTahu = 'cross';
+  } else {
+    btnElm[i].classList.add('pole-cross');
+    kdoJeNaTahu = 'circle';
+  }
+
+  hrac.innerHTML = Hraje();
+
+});
+  
+
+};
